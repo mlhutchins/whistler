9ad6e12b96a6a44437a29bb9e819d2be74a2ebc0
Michael Hutchins <mlhutch@uw.edu>
2013-10-30 16:29:38 -0700

    Empty function to get dispersion.

---
+++ dispersion_check.m
@@ -0,0 +1,10 @@
+function [D, time] = dispersion_check(timeBase, freqWindow, spectra)
+%DISPERSION_CHECK calculates the best fit dispersion and arrival time of
+%the isolated sferic in spectra
+%
+%	Written by: Michael Hutchins
+
+
+
+end
+
