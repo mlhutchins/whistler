6fd158c429c706d4357e0a7965ec15a2d950a4af
Michael Hutchins <mlhutch@uw.edu>
2014-01-03 15:06:12 -0800

    Function to get uniformly shaped spectra from a wideband file and trigger time.

--- neural_network_training.m
+++ neural_network_training.m
@@ -38,3 +38,54 @@ function neural_network_training
 
 
 end
+
+function [ spectra ] = whistler_spectra( widebandFile, startTime )
+%WHISTLER_SPECTRA takes the FFT of wideband data y and returns the 
+%	portion of the spectra between 0 - 13 kHz as a 2D array
+%
+%	Written by: Michael Hutchins
+
+%% Set start/end time buffers
+
+	startBuffer = 0.1; %seconds
+	endBuffer = 1.9; %seconds
+
+%% Import wideband file
+
+	[time, eField, Fs] = wideband_import(widebandFile);
+	
+%% Get spectral power density
+
+	[timeBase,freqBase,power] = wideband_fft(eField,Fs);
+
+%% Cut down in frequency and space
+
+	spectra = power(freqBase < 13000,...
+					timeBase > startTime - startBuffer &...
+					timeBase < startTime + endBuffer);
+				
+%% Add padding as necessary
+
+	padValue = mean(spectra(:));
+	expectedSize = round((startBuffer + endBuffer) * Fs);
+	actualSize = size(spectra,2);
+
+	if actualSize < expectedSize
+		
+		padAmount = expectedSize - actualSize;
+		
+		padding = repmat(padValue,size(spectra,1),padAmount);
+		
+		if (timeBase(end) < (startTime + endBuffer));
+		
+			spectra = [padding, spectra];
+		
+		else 
+		
+			spectra = [spectra, padding];
+		end
+	
+	end
+
+end
+
