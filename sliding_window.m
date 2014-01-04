55fefff3ccee92aba961ed3db317eec1501f55be
Michael Hutchins <mlhutch@uw.edu>
2014-01-03 21:12:54 -0800

    Expanded output of sliding_window with a cell array of located whistler spectrograms.

--- sliding_window.m
+++ sliding_window.m
@@ -1,4 +1,4 @@
-function [ location ] = sliding_window( eField, Fs, cutoff )
+function [ location, spectra, freqBase, timeBase ] = sliding_window( eField, Fs, cutoff )
 %SLIDING_WINDOW searches a wideband file for whistlers using a neural network
 %	classifier
 %
@@ -16,23 +16,29 @@ function [ location ] = sliding_window( eField, Fs, cutoff )
 	
 	probability = zeros(length(windows),1);
 	
+	spectra = cell(length(windows),1);
+	
 	%% Chech each window
 	
 	for i = 1 : length(windows);
 
 		%% Check each window
 		
-		[ spectra ] = whistler_spectra( timeBase, freqBase, power, windows(i) );
+		[ spec ] = whistler_spectra( timeBase, freqBase, power, windows(i) );
 
+		spectra{i} = spec;
+		
 		%% Find local probability maxima above threshold
 
-		probability(i) = predict_whistler(Theta1, Theta2, spectra(:)');
+		probability(i) = predict_whistler(Theta1, Theta2, spec(:)');
 
 	end
 	
 	%% Count and locate whistlers
 	
-	location = findpeaks(probability,'minpeakheights',cutoff,...
+	[location, idx] = findpeaks(probability,'minpeakheights',cutoff,...
 									 'minpeakdistance',3);
 	
+	spectra = spectra(idx);
+								 
 end
