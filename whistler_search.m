d5ce6f8183c0764600eb23b199e0c3dd2329a641
Michael Hutchins <mlhutch@uw.edu>
2014-01-03 21:13:21 -0800

    Whistler search wideband file listing from the input directory.

--- whistler_search.m
+++ whistler_search.m
@@ -6,6 +6,19 @@ function [ times, dispersion ] = whistler_search( directory )
 
 	%% Get file list
 	
+	fileList = dir(directory);
+	index = 1;
+	
+	for i = 1 : length(fileList);
+		
+		entry = fileList(i).name;
+		
+		if strcmp(entry(1:2),'WB') && strcmp(entry(end-2 : end),'dat')
+			files{index} = fileList(i).name;
+			index = index + 1;
+		end
+		
+	end
 	
 	%% Initalize parameters
 	
