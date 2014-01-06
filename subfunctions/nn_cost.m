0c82dd54fbd85dc03a1b0cc66a212fa81b0d8046
Michael Hutchins <mlhutch@uw.edu>
2014-01-06 14:39:08 -0800

    Fixed the unrolling of nnParams.

--- subfunctions/nn_cost.m
+++ subfunctions/nn_cost.m
@@ -32,30 +32,30 @@ function [J, grad] = nn_cost(nnParams, ...
 			b = (inputLayerSize + 1); % First layer size + 1 (bias)
 			c = hiddenLayerSize(i); % Next layer size
 
-			d = a + b * c;
+			d = a + b * c - 1;
 			index = d;
 			
 		elseif i < nLayers
 			
 			a = index + 1; % Start point
-			b = (hiddenLayerSize(i) + 1); % Current layer size + 1 (bias)
-			c = hiddenLayerSize(i + 1); % Next layer size
+			b = (hiddenLayerSize(i - 1) + 1); % Current layer size + 1 (bias)
+			c = hiddenLayerSize(i + 1 - 1); % Next layer size
 
-			d = a + b * c;
+			d = a + b * c - 1;
 			index = d;
 			
 		elseif i == nLayers
 
 			a = index + 1; % Start point
-			b = (hiddenLayerSize(i) + 1); % Current layer size + 1 (bias)
+			b = (hiddenLayerSize(i - 1) + 1); % Current layer size + 1 (bias)
 			c = nLabels; % Next layer size
 
-			d = a + b * c;
+			d = a + b * c - 1;
 			index = d;
 				
 		end
 					
-		Theta{i} = reshape(nnParams(a:b),c,d);
+		Theta{i} = reshape(nnParams(a:d),b,c);
 
 		
 	end
