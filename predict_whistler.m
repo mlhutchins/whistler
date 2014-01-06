35b65bbdd8aa9907918031d2212b3f3e0409e846
Michael Hutchins <mlhutch@uw.edu>
2014-01-06 15:51:18 -0800

    Updated predict to use the new Theta structure.

--- predict_whistler.m
+++ predict_whistler.m
@@ -1,4 +1,4 @@
-function p = predict_whistler(Theta1, Theta2, X)
+function p = predict_whistler(Theta, X)
 %PREDICT Predict the label of an input given a trained neural network
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
@@ -10,10 +10,40 @@ function p = predict_whistler(Theta1, Theta2, X)
 	m = size(X, 1);
 
 %% Get predicted output label index
+	
+	nLayers = length(Theta);
 
-	h1 = sigmoid([ones(m, 1) X] * Theta1');
-	h2 = sigmoid([ones(m, 1) h1] * Theta2');
-	[~, p] = max(h2, [], 2);
+	% Forward propagate network to get h(theta)
+
+	z{nLayers} = [];
+	a = z;
+	
+	z{1} = X;
+	
+	for i = 1 : nLayers + 1
+		
+		if i == 1;
+			
+			z{i} = X;
+			
+		else
+		
+			zPrime = a{i - 1} * Theta{i - 1};
+		
+			z{i} = sigmoid(zPrime);
+		
+		end
+		
+		
+		a{i} = [ones(m,1), z{i}];
+				
+	end
+	
+	a{end} = a{end}(:,2:end);
+	
+	h = a{end};
+
+	[~, p] = max(h, [], 2);
 	
 %% Set to positive/negetive index
 
