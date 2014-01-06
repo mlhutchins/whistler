86856603f94e027a6170a89e18971f783c9526da
Michael Hutchins <mlhutch@uw.edu>
2014-01-06 14:40:52 -0800

    Updated the unrolling in the main training script.

--- neural_network_training.m
+++ neural_network_training.m
@@ -187,8 +187,8 @@ function neural_network_training
 	[nnParams, cost] = fmincg(costFunction, initialParams, options);
 
 	% Obtain Theta1 and Theta2 back from nnParams
-	
-	Theta = cell(nLayers,1);
+
+	Theta{nLayers} = [];
 	
 	for i = 1 : nLayers;
 		
@@ -198,31 +198,31 @@ function neural_network_training
 			b = (inputLayerSize + 1); % First layer size + 1 (bias)
 			c = hiddenLayerSize(i); % Next layer size
 
-			d = a + b * c;
+			d = a + b * c - 1;
 			index = d;
 			
 		elseif i < nLayers
 			
 			a = index + 1; % Start point
-			b = (hiddenLayerSize(i) + 1); % Current layer size + 1 (bias)
-			c = hiddenLayerSize(i + 1); % Next layer size
+			b = (hiddenLayerSize(i - 1) + 1); % Current layer size + 1 (bias)
+			c = hiddenLayerSize(i + 1 - 1); % Next layer size
 
-			d = a + b * c;
+			d = a + b * c - 1;
 			index = d;
 			
 		elseif i == nLayers
 
 			a = index + 1; % Start point
-			b = (hiddenLayerSize(i) + 1); % Current layer size + 1 (bias)
+			b = (hiddenLayerSize(i - 1) + 1); % Current layer size + 1 (bias)
 			c = nLabels; % Next layer size
 
-			d = a + b * c;
+			d = a + b * c - 1;
 			index = d;
 				
 		end
 					
-		Theta{i} = reshape(nnParams(a:b),c,d);
-		
+		Theta{i} = reshape(nnParams(a:d),b,c);
+
 	end
 	
 	save('trainedNeuralNet','-v7.3');
