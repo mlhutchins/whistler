9c20b8f626117bb4f6608c0b57a259eaf7148d3f
Michael Hutchins <mlhutch@uw.edu>
2014-01-08 14:14:24 -0800

    Wrote a basic unit test for the neural network training.

---
+++ unit_test.m
@@ -0,0 +1,29 @@
+function [ success ] = unit_test
+% UNIT_TEST runs the neural network code with the basic OCR dataset to check
+%	functionality of code
+%
+%	Written by: Michael Hutchins
+
+	%% Load OCR data
+	
+	load ocr_data
+	
+	%% Format into a stack of images
+
+	samples = reshape(X,5000,20,20);
+	labels = y;
+	
+	data
+	
+	clear X y
+	
+	%% Initialize neural network
+	
+	neuralNetwork = neural_network_init();
+	
+	%% Train neural network
+	
+	[Theta, statistics] = neural_network_training(samples,labels,neuralNetwork);
+	
+end
+
