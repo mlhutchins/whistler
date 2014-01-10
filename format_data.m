2a6ab8cb7ae3574358bc2feae46987fb2c2216ae
Michael Hutchins <mlhutch@uw.edu>
2014-01-10 11:37:48 -0800

    Format data now takes in the frequency range and percentile threshold value.

--- format_data.m
+++ format_data.m
@@ -1,11 +1,17 @@
-function [ samples, nWidth ] = format_data( images )
+function [ samples, nWidth ] = format_data( images, threshold, freqThreshold )
 %FORMAT_DATA(images) formats and unwraps images for input into a neural network
 %
 %	Written by: Michael Hutchins
 
+	switch nargin
+		case 1
+			threshold = 85;
+			freqThreshold = [4000, 4500];
+	end
+
 	frequency = linspace(1000,10000,size(images,2));
 
-	images = images(:,frequency > 4000 & frequency < 4500,:);
+	images = images(:,frequency > freqThreshold(1) & frequency < freqThreshold(2),:);
 	
 	samples = zeros(size(images,1), size(images,2) * size(images,3));
 
@@ -13,7 +19,6 @@ function [ samples, nWidth ] = format_data( images )
 	for i = 1 : size(images,1);
 
 		spectra = squeeze(images(i,:,:));
-		
 
 		%% Formatting code
 		
@@ -25,7 +30,7 @@ function [ samples, nWidth ] = format_data( images )
 		spectra(spectra < minPower) = minPower;
 		spectra(spectra > maxPower) = maxPower;
 
-		spectra = spectra > prctile(spectra(:),85);
+		spectra = spectra > prctile(spectra(:),threshold);
 	
 		samples(i,:) = spectra(:)';
 		
