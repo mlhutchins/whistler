34b6122a322756776e92c737e445fee69542f0bf
Michael Hutchins <mlhutch@uw.edu>
2014-03-03 12:05:46 -0800

    Updated format data to reflect final settings.

--- format_data.m
+++ format_data.m
@@ -8,8 +8,8 @@ function [ samples, nWidth ] = format_data( images, threshold, freqThreshold )
 
 	switch nargin
 		case 1
-			threshold = 85;
-			freqThreshold = [4, 4.5]; %kHz
+			threshold = 45;
+			freqThreshold = [1, 6.5]; %kHz
 	end
 	
 	if length(size(images)) == 2
